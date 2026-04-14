f331b1d38d9bfa42dcd2d1fcb6a10b1f841565e8
Max Patiiuk <max@patii.uk>
2026-04-14 10:59:54 -0700

    chore: add initial boilerplate

---
+++ window-manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  window-manager
+//
+//  Created by Max Patiiuk on 2026-04-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world! 🌏")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ window-manager/window_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  window_managerApp.swift
+//  window-manager
+//
+//  Created by Max Patiiuk on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct window_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
